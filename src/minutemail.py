8624df57254c8ba24bbbe3fca5efd5196e6c8f25
Diego <taquitoslayer@gmail.com>
2024-02-16 00:45:30 -0700

    adding more to headers for possible 403 fix

--- src/minutemail.py
+++ src/minutemail.py
@@ -2,6 +2,7 @@ import requests
 from endpoints import NEW_EMAIL, MESSAGE_AFTER, MESSAGE_COUNT
 from fake_useragent import UserAgent
 
+
 class Mail(object):
     """
     Python wrapper for 10minutemail.com
@@ -9,7 +10,24 @@ class Mail(object):
 
     def __init__(self):
         self.session = requests.session()
-        self.session.headers = {'user-agent': UserAgent().random}
+        self.session.headers = {
+            'authority': '10minutemail.com',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-CA,en-US;q=0.9,en;q=0.8',
+            'cache-control': 'no-cache',
+            'dnt': '1',
+            'pragma': 'no-cache',
+            'referer': 'https://www.bing.com/',
+            'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+        }
         self.message_count = 0
         self.messages = []
         self.mail = self.session.get(NEW_EMAIL).json()['address']
