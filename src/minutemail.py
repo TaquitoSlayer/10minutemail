8c6cd7e3e57da53b2a30927dd32c8aae8fc66a65
Diego <taquitoslayer@gmail.com>
2024-02-16 00:47:57 -0700

    ---

--- src/minutemail.py
+++ src/minutemail.py
@@ -12,21 +12,20 @@ class Mail(object):
         self.session = requests.session()
         self.session.headers = {
             'authority': '10minutemail.com',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept': '*/*',
             'accept-language': 'en-CA,en-US;q=0.9,en;q=0.8',
             'cache-control': 'no-cache',
             'dnt': '1',
             'pragma': 'no-cache',
-            'referer': 'https://www.bing.com/',
+            'referer': 'https://10minutemail.com/',
             'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'cross-site',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+            'x-requested-with': 'XMLHttpRequest',
         }
         self.message_count = 0
         self.messages = []
